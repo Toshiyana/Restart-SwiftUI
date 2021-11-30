59202873b777f7726638a196f2cdb404c59bd328
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-30 16:47:05 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Toshiyana on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
